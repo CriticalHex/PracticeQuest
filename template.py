a520286c8ca2011fb406d9c1785ef864707fa242
CriticalHex <80023875+CriticalHex@users.noreply.github.com>
2022-04-30 06:09:31 -0600

    Ready

--- template.py
+++ template.py
@@ -2,20 +2,7 @@ import sys
 import math
 import string
 
-def bround(num):
-    decimals = 100.0
-    multiplied = num * decimals
-    floored = math.floor(multiplied)
-    remainder = multiplied - floored
-    if remainder >= .5:
-        floored += 1
-    rounded = str(floored / decimals)
-    while len(rounded) - rounded.index('.') <= 2:
-        rounded += '0'
-    return rounded
-
 cases = int(sys.stdin.readline().rstrip())
 
 for caseNum in range(cases):
     line = sys.stdin.readline().rstrip()
-    var1, var2 = (float(val) for val in line.split(","))
